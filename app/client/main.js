dd4ff840aa66a9ff6adaa22f750dd3939ab7dfaa
Yui Wei <tyuiwei@gmail.com>
2017-02-27 23:06:53 +0800

    finally upload to server working

--- app/client/main.js
+++ app/client/main.js
@@ -3,20 +3,50 @@ import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
+Template.uploadedFiles.helpers({
+  uploadedFiles: function () {
+    return Images.find();
+  }
 });
 
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
+Template.uploadForm.onCreated(function () {
+  this.currentUpload = new ReactiveVar(false);
 });
 
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
+Template.uploadForm.helpers({
+  currentUpload: function () {
+    return Template.instance().currentUpload.get();
+  }
+});
+
+Template.uploadForm.events({
+  'change #fileInput': function (e, template) {
+    if (e.currentTarget.files && e.currentTarget.files[0]) {
+      // We upload only one file, in case
+      // there was multiple files selected
+      var file = e.currentTarget.files[0];
+      if (file) {
+        var uploadInstance = Images.insert({
+          file: file,
+          streams: 'dynamic',
+          chunkSize: 'dynamic'
+        }, false);
+
+        uploadInstance.on('start', function() {
+          template.currentUpload.set(this);
+        });
+
+        uploadInstance.on('end', function(error, fileObj) {
+          if (error) {
+            alert('Error during upload: ' + error.reason);
+          } else {
+            alert('File "' + fileObj.name + '" successfully uploaded');
+          }
+          template.currentUpload.set(false);
+        });
+
+        uploadInstance.start();
+      }
+    }
+  }
 });
--- app/server/main.js
+++ app/server/main.js
@@ -1,5 +1,32 @@
 import { Meteor } from 'meteor/meteor';
+import { WebApp } from 'meteor/webapp';
+var fs = Npm.require('fs');
+var exec = Npm.require('child_process').exec;
+var Fiber = Npm.require('fibers');
+var Future = Npm.require('fibers/future');
 
 Meteor.startup(() => {
   // code to run on server at startup
+
+});
+
+
+
+Meteor.methods({
+
+  callPython: function() {
+    var fut = new Future();
+    exec('pythonScriptCommand with parameters', function (error, stdout, stderr) {
+
+      // if you want to write to Mongo in this callback
+      // you need to get yourself a Fiber
+      new Fiber(function() {
+        ...
+        fut.return('Python was here');
+      }).run();
+
+    });
+    return fut.wait();
+  },
+
 });
